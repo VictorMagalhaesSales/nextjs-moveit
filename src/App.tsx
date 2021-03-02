f921094f45a47fd7496e10aee321ef729ce26e37
Victor Magalhães <victormagalhaessales@gmail.com>
2021-03-02 03:05:42 -0300

    Criando  primeiro componente de ExperienceBar com estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar'
+import './styles/global.css'
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div className="container">
+      <ExperienceBar/>
+    </div>
   );
 }
 
